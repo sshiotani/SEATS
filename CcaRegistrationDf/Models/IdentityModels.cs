301ed227646b94178a097a1d622f8cd771d178ff
Spencer Shiotani <spencer.shiotani@schools.utah.gov>
2015-05-12 08:48:41 -0600

    Latest

--- CcaRegistrationDf/Models/IdentityModels.cs
+++ CcaRegistrationDf/Models/IdentityModels.cs
@@ -32,22 +32,16 @@ namespace CcaRegistrationDf.Models
             return new ApplicationDbContext();
         }
 
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Parent> Parents { get; set; }
-        public DbSet<OnlineCourse> Courses { get; set; }
-        public DbSet<Provider> Providers { get; set; }
-        public DbSet<Primary> Primaries { get; set; }
-        public DbSet<Counselor> Counselors { get; set; }
-        public DbSet<Session> ClassSession { get; set; }
-        public DbSet<CourseCategory> CourseCategories { get; set; }
-        public DbSet<CourseCredit> CourseCredits { get; set; }
-        public DbSet<CCA> CCAs { get; set; }
-
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.Student> Students { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.Parent> Parents { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.OnlineCourse> Courses { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.Provider> Providers { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.Primary> Primaries { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.Counselor> Counselors { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.Session> ClassSession { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.CourseCategory> CourseCategories { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.CourseCredit> CourseCredits { get; set; }
+        public System.Data.Entity.DbSet<CcaRegistrationDf.Models.CCA> CCAs { get; set; }
 
         
     }
