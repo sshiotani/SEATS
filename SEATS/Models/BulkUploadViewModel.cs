bcd7bbd4a04cd83dadc7bfb93e835900c95646cd
Spencer Shiotani <spencer.shiotani@schools.utah.gov>
2015-07-30 14:34:34 -0600

    Upload student

---
+++ SEATS/Models/BulkUploadViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SEATS.Models
+{
+    public class BulkUploadViewModel
+    {
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
