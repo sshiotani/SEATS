30aa40dd03def0b75571b7328ddf5fd385cd32c5
Spencer Shiotani <spencer.shiotani@schools.utah.gov>
2015-08-04 12:47:53 -0600

    New Cactus

--- SEATS/Models/Cactus.Designer.cs
+++ SEATS/Models/Cactus.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\sshiotani\documents\visual studio 2013\Projects\CcaRegistrationDf\CcaRegistrationDf\Models\Cactus.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\sshiotani\Documents\Visual Studio 2013\Projects\SEATS\SEATS\Models\Cactus.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
